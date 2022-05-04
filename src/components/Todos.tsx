cf66e7dc9ad16043e07b87b62ff59298cb8a4238
himoura32 <shawn.ahearn@outlook.com>
2022-05-04 14:32:43 -0400

    coded refs, function props, state/remove todo

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React from 'react';
 
-const Todos: React.FC<{ items: string[] }> = (props) => {
+import TodoItem from './TodoItem';
+import Todo from '../models/todo';
+import classes from './Todos.module.css';
+
+const Todos: React.FC<{ items: Todo[]; onRemoveTodo: (id: string) => void }> = (
+  props
+) => {
   return (
-    <ul>
+    <ul className={classes.todos}>
       {props.items.map((item) => (
-        <li key={item}>{item}</li>
+        <TodoItem
+          key={item.id}
+          text={item.text}
+          onRemoveTodo={props.onRemoveTodo.bind(null, item.id)}
+        />
       ))}
     </ul>
   );
